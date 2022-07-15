03a584611206b91f3c4794f73cb587093732ab44
salwa <salwa_hamim@hotmail.com>
2022-07-15 16:10:06 -0700

    put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<string.h>
-int main() {
-int puts (const char *str)
-	char str1[] = "\"Programming is like building a multilingual puzzle";
+int main(void) 
+{
+char str[] = "\""Programming is like building a multilingual puzzle";
 puts(str1);
 return (0);
 }
