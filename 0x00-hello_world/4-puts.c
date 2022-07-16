c92b413f4ca0ac026b5c9965f3c5b36adf2ca5fa
salwa <salwa_hamim@hotmail.com>
2022-07-16 01:33:09 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-/*
- *
- *  * main - Entry point
- *
- *   * Description: 'C program for puts char function'
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+/**
+* main - Entry point
+* Description: 'C program for puts char function'
+* Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
