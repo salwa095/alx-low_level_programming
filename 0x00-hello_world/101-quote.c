08aa84221032017275feafe74bf6c9229a573a1f
salwa <salwa_hamim@hotmail.com>
2022-07-16 12:41:47 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+
 /**
 * Main - main block
 * Description: prints exactly "and that piece of art is useful"
@@ -10,6 +11,6 @@
 int main(void)
 {
 int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", s);
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", s);
 return (1);
 }
