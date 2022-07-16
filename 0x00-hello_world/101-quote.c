3a78cfec9bd649e6667f85e78de95aad6f7ed41f
salwa <salwa_hamim@hotmail.com>
2022-07-16 12:22:43 -0700

    printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 /**
-* Entry: starting point of c
-* Description: 'starting from main and then proceed to printing characters without using print'
+* Main - starting point of c
+* Description - 'printing characters without using print'
 * Return: 0 is success
 */
 int main(void)
 {
-write(1,"and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n",59);
+int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", s);
 return (1);
 }
