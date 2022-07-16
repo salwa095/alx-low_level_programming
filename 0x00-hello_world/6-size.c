b35360f5612073e77ac0577a947bef104b894afb
salwa <salwa_hamim@hotmail.com>
2022-07-16 05:19:11 -0700

    print various data types sizes using sizeof command

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,10 @@
 
 int main(void)
 {
-printf("size of int is: %i byte(s)\n", sizeof(int));
 printf("size of char is: %i byte(s)\n", sizeof(char));
-printf("size of double is: %i byte(s)\n", sizeof(double));
-printf("size of float is: %i byte(s)\n", sizeof(float));
-printf("size of long long int is: %i byte(s)\n", sizeof(long long int));
+printf("size of int is: %i byte(s)\n", sizeof(int));
 printf("size of long is: %i byte(s)\n", sizeof(long));
+printf("size of long long int is: %i byte(s)\n", sizeof(long long int));
+printf("size of float is: %i byte(s)\n", sizeof(float));
 return (0);
 }
