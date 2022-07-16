a785e12f609b4ebbd281f9104014e5f1c99d609f
salwa <salwa_hamim@hotmail.com>
2022-07-16 05:12:07 -0700

    printf different data types sizes on different machines

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,16 @@
 /**
 * main point
 * description: 'printing size of various data types'
-* returns 0
+* returns: 0 is successfdully
 */
+
 int main(void)
 {
-printf("size of int is: %i\n", sizeof(int));
-printf("size of char is: %i\n", sizeof(char));
-printf("size of double is: %i\n", sizeof(double));
-printf("size of float is: %i\n", sizeof(float));
-printf("size of long is: %i\n", sizeof(long));
+printf("size of int is: %i byte(s)\n", sizeof(int));
+printf("size of char is: %i byte(s)\n", sizeof(char));
+printf("size of double is: %i byte(s)\n", sizeof(double));
+printf("size of float is: %i byte(s)\n", sizeof(float));
+printf("size of long long int is: %i byte(s)\n", sizeof(long long int));
+printf("size of long is: %i byte(s)\n", sizeof(long));
 return (0);
 }
