2dd29cb1bd5a15dca1ce93698cdb621769536c5b
salwa <salwa_hamim@hotmail.com>
2022-07-16 11:41:00 -0700

    print characters without printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
-* Entry - starting point of c
+* Entry: starting point of c
 * Description: 'starting from main and then proceed to printing characters without using print'
 * Return: 0 is success
 */
-
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-return (0);
+write(1,"and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n",59);
+return (1);
 }
