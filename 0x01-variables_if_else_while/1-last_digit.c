42a8ac9b926c10f5bbe8334962fc293e2ff386ee
salwa <salwa_hamim@hotmail.com>
2022-07-18 07:18:43 -0700

    display random numbers and check whether it is greater than 5, less than 6 or equal to zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,31 @@
-#include <stdio.h>                                                                                                                                    
-#include <time.h>                                                                                                                                     
-#include <stdlib.h>                                                                                                                                   
-/**                                                                                                                                                   
-* main - main block                                                                                                                                   
-* Description: 'Print a random number and print if positive or negative'                                                                              
-* Return: 0                                                                                                                                           */                                                                                                                                      
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+* main - The starting point in C program
+* Description: -Print random numbers and find the last digit'
+* Return: 0
+*/
+
 int main(void)
-{                                                                                                                                                     
-int n;                                                                                                                                                
-srand(time(0));                                                                                                                                       
-n = rand() - RAND_MAX / 2;                                                                                                                            if(n > 5)                                                  
-printf("Last digit of %d is and is greater than 5\n", n);                                                                                             else if(n == 0)                                                 
-printf("Last digit of %d is and is 0\n", n);
-else(n < 6 != 0) 
-printf("Last digit of %d is and is less than 6 and not 0\n", n);                                                                                      return (0);                                                                                        
+{
+int n;
+int last;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last = n % 10;
+if (last > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
+}
+else if (last == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, last);
+}
+else if (last < 6 && last != 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+}
+return (0);
 }
