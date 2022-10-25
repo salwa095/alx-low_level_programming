4ba5c39fdc04a43a5ea6d9a9a43e924b28f483d1
Salwa <salwa_hamim@hotmail.com>
2022-10-25 11:45:20 -0700

    lowercase letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ *
+ * * main - the starting point in C program
+ *
+ * * Description: -Print 
+ *
+ * * return: 0
+ *
+ * */
+
+int main(void) 
+{
+int ch='a';
+putchar(ch);
+
+return (0);
+}
