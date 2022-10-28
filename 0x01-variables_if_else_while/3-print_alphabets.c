7ba8e1408d30f08c041a25a762a901b26be98fe9
Salwa <salwa_hamim@hotmail.com>
2022-10-28 06:29:28 -0700

    printing lowercase with uppercase followed by new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int x;
+
+for(x = 'a'; x <= 'z'; x++)
+
+putchar(x);	
+
+for(x = 'A'; x <= 'Z'; x++)
+
+putchar(x);
+return (0);
+}
