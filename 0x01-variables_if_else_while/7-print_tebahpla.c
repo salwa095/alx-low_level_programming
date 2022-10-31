24b6a6812343d60a996a136ad6e459a98e335063
Salwa <salwa_hamim@hotmail.com>
2022-10-31 15:14:53 -0700

    smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,9 +7,9 @@
  */
 int main(void) 
 {
-char l;
-for (l = 'z'; l <= 'a'; l++)
-putchar('l');
+char letter;
+for (letter = 'z'; letter <= 'a'; letter--)
+putchar(letter);
 putchar('\n');
 return (0);
 }
