6e313cbcb21611ef2bde07f20537d0b2cc505362
Salwa <salwa_hamim@hotmail.com>
2022-10-31 23:09:15 -0700

    base 16 numbers and alphbets

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main  - Entry point
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int a;
+char b;
+for (a = 0; a <= 15; a++)
+putchar((a%10) + '0');
+for (b = 'a'; b <= 'f'; b++)
+putchar(b);
+putchar('\n');
+return (0);
+}
