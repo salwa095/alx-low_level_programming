d99dd5caf3a27c8c9cf45e5aa890613558a9ffd2
Salwa <salwa_hamim@hotmail.com>
2022-11-01 10:35:51 -0700

    print possbile combinations of three digits between 00 and 99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,19 @@ int main(void)
 {
 int n1, n2;
 
-for (n1 = 0; n1 < 98; n1++)
+for (n1 = 0; n1 <= 98; n1++)
 {
 
-for (n2 = n1 + 1; n2 < 99; n2++)
+for (n2 = n1 + 1; n2 <= 99; n2++)
 {
-if (n1 == 97 && n2 == 98 )
-continue;
 putchar((n1 / 10) + '0');
 putchar((n1 % 10) + '0');
 putchar(' ');
 putchar((n2 / 10) + '0');
 putchar((n2 % 10) + '0');
 
+if (n1 == 98 && n2 == 99)
+continue;
 
 putchar(',');
 putchar(' ');
