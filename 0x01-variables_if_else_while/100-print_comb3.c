db6b13a1bd296d9fb54fccc6563d322f4dafc195
Salwa <salwa_hamim@hotmail.com>
2022-11-01 03:36:52 -0700

    print possible double digits using putchar

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always success (0)
+ */
+int main(void)
+{
+int num, num2;
+for (num = 0; num <= 9; num++)
+{
+for (num2 = num + 1; num2 <= 10; num2++)
+{
+putchar((num % 10) + '0');
+putchar((num2 % 10) + '0');
+if (num == 8 && num2 == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
