a6aa72265a7adfad5ee87270e7412418a396b055
Salwa <salwa_hamim@hotmail.com>
2022-11-06 00:47:02 -0700

    added main header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,6 @@
  * main - prints together
  * Return: always 0
  */
-
 int main(void)
 {
 _putchar('_');
@@ -15,5 +14,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
+_putchar('\n');
 return (0);
 }
