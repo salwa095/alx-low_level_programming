4c4c5d8e57e0f90cac92c9d0c0cf876731820dfc
Salwa <salwa_hamim@hotmail.com>
2022-11-15 09:32:51 -0800

    dpne

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,17 @@
 
 /**
  * print_last_digit - function that prints the last digit of a number
+ * @b: passed argument
  * Return: returns last value of the digit
  */
 int print_last_digit(int b)
 {
 int s;
+if (b < 0)
+{
+b = -b;
 s = (b % 10);
 _putchar(s + '0');
 return (s);
+}
 }
