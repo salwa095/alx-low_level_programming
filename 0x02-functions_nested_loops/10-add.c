8fb3a9944a0951d9701536e874f8f6bb6085231b
Salwa <salwa_hamim@hotmail.com>
2022-11-16 00:14:47 -0800

    done

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 
 /**
  * add - function that adds two integers and returns the result
- * a = first integer , b = second integer
+ * @a = first integer, @b = second integer
  * Return: 0 if success
  */
 int add(int a, int b)
