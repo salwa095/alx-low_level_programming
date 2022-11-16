0ba301b40ddfc2681c259799f31a709f20c26b51
Salwa <salwa_hamim@hotmail.com>
2022-11-15 23:15:59 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * time_table - function that prints 9 times table starting with 0
+ * Return
+ */
+void times_table(void)
+{
+int t;
+for (t = 0; t <= 90; t++)
+{
+_putchar(t + '0');
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -26,3 +26,5 @@ int _abs(int a);
 int print_last_digit(int b);
 
 void jack_bauer(void);
+
+void times_table(void);
