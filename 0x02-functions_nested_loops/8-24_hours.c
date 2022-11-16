8ad4e9bb8d76d49835199ee3abf6eb1875ebc96f
Salwa <salwa_hamim@hotmail.com>
2022-11-15 23:05:46 -0800

    done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * 24_hours - function that prints every minute of the day
+ * h = hours, m = minutes
+ * Return: Always 0 if success
+ */
+void jack_bauer(void);
+{
+int h, m;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar('\n');
+}
