1f6ab3ed3683180671b26513af376dcb6b0c8467
Salwa <salwa_hamim@hotmail.com>
2022-11-19 03:46:18 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,21 @@
  */
 void times_table(void)
 {
-int t;
-for (t = 0; t <= 90; t++)
+int num, mult, prod;
+for (num = 0; num <= 9; num++)
 {
-_putchar(t + '0');
+_putchar('0');
+for (mult = 1; mult <= 9; mult++)
+{
+_putchar(',');
+_putchar(' ');
+prod = num * mult;
+if (prod <= 9)
+_putchar(' ');
+else
+_putchar((prod / 10) + '0');
+_putchar((prod % 10) + '0');
+}
+_putchar((prod % 10) + '0');
 }
 }
